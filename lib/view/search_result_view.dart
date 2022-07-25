a73fd0103e32ce35fb57f2ff9290a6d4d7237cf2
kiyokawakouji <kohji12k@gmail.com>
2022-07-26 01:05:12 +0900

    リストアイテムの作成

--- lib/view/search_result_view.dart
+++ lib/view/search_result_view.dart
@@ -10,7 +10,7 @@ class SearchResultView extends StatefulWidget {
 }
 
 class _SearchResultViewState extends State<SearchResultView> {
-  List<Color> colorList = [Colors.cyan, Colors.tealAccent, Colors.greenAccent];
+  // List<Color> colorList = [Colors.cyan, Colors.tealAccent, Colors.greenAccent];
 
   @override
   Widget build(BuildContext context) {
@@ -22,48 +22,199 @@ class _SearchResultViewState extends State<SearchResultView> {
           child: _SearchBarWidget(),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Center(child: ListView.builder(
-          itemBuilder: (BuildContext context, int index) {
-            return Container(
-              height: 80,
-              color: colorList[index % colorList.length],
-            );
-          },
-        )),
-      ),
+      body: const MyStatelessWidget(),
+      // body: Padding(
+      //   padding: const EdgeInsets.all(16),
+      //   child: Center(
+      //     child: ListView.builder(
+      //       itemBuilder: (BuildContext context, int index) {
+      //         return Container(
+      //           height: 80,
+      //           color: colorList[index % colorList.length],
+      //         );
+      //       },
+      //     ),
+      //   ),
+      // ),
     );
   }
+}
 
-  Widget _SearchBarWidget() {
-    return Container(
-      margin: const EdgeInsets.all(16),
-      child: const TextField(
-        decoration: InputDecoration(
-          filled: true,
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.all(
-              Radius.circular(40),
-            ),
-            borderSide: const BorderSide(
-              width: 0,
-              style: BorderStyle.none,
-            ),
+class _ArticleDescription extends StatelessWidget {
+  const _ArticleDescription({
+    Key? key,
+    required this.title,
+    required this.subtitle,
+    required this.author,
+    required this.publishDate,
+    required this.readDuration,
+  }) : super(key: key);
+
+  final String title;
+  final String subtitle;
+  final String author;
+  final String publishDate;
+  final String readDuration;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                title,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Padding(padding: EdgeInsets.only(bottom: 2)),
+              Text(
+                subtitle,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontSize: 12,
+                ),
+              ),
+            ],
           ),
-          prefixIcon: Icon(Icons.search),
-          hintText: 'Search for a project.',
         ),
-        // onChanged: (inputString) {
-        //   if (inputString.length >= 5) {
-        //     _searchRepositories(inputString).then((repositories) {
-        //       setState(() {
-        //         _repositories = repositories;
-        //       });
-        //     });
-        //   }
-        // },
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Text(
+                author,
+                style: const TextStyle(
+                  fontSize: 12,
+                ),
+              ),
+              Text(
+                '$publishDate - $readDuration',
+                style: const TextStyle(
+                  fontSize: 12,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class CustomListItemTwo extends StatelessWidget {
+  const CustomListItemTwo({
+    Key? key,
+    required this.title,
+    required this.subtitle,
+    required this.author,
+    required this.publishDate,
+    required this.readDuration,
+  }) : super(key: key);
+
+  final String title;
+  final String subtitle;
+  final String author;
+  final String publishDate;
+  final String readDuration;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Container(
+        height: 100,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.white),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.grey,
+              spreadRadius: 1,
+              blurRadius: 10,
+              offset: Offset(5, 5),
+            ),
+          ],
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(20, 0, 2, 0),
+                child: _ArticleDescription(
+                  title: title,
+                  subtitle: subtitle,
+                  author: author,
+                  publishDate: publishDate,
+                  readDuration: readDuration,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: const EdgeInsets.all(10),
+      children: const [
+        CustomListItemTwo(
+          title: 'Flutter 1.0 Launch',
+          subtitle: 'Flutter continues to improve and expand its horizons. '
+              'This text should max out at two lines and clip',
+          author: 'Dash',
+          publishDate: 'Dec 28',
+          readDuration: '5 mins',
+        ),
+      ],
+    );
+  }
+}
+
+Widget _SearchBarWidget() {
+  return Container(
+    margin: const EdgeInsets.all(16),
+    child: const TextField(
+      decoration: InputDecoration(
+        filled: true,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(40),
+          ),
+          borderSide: BorderSide(
+            width: 0,
+            style: BorderStyle.none,
+          ),
+        ),
+        prefixIcon: Icon(Icons.search),
+        hintText: 'Search for a project.',
+      ),
+      // onChanged: (inputString) {
+      //   if (inputString.length >= 5) {
+      //     _searchRepositories(inputString).then((repositories) {
+      //       setState(() {
+      //         _repositories = repositories;
+      //       });
+      //     });
+      //   }
+      // },
+    ),
+  );
 }
